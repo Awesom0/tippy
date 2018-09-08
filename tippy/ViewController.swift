1e550d94090b5ae76cb80fedbd779ce552afd2b8
Felipe De La Torre <felipedelatorre@Felipes-MacBook-Pro.local>
2018-09-07 18:23:49 -0700

    Tip Calculator files

---
+++ tippy/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Felipe De La Torre on 9/5/18.
+//  Copyright © 2018 Felipe De La Torre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func Ontap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+}
+
